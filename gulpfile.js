d8b17b05f519a15520d1112d0fe5b99188f856ff
agdwm <almudenaguerraswm@gmail.com>
2018-05-03 22:51:07 +0200

    00 - Created a Basic Boilerplate to customize Bootstrap Using bootstrap-sass and Gulp

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+// Source and Distribution Folders
+const source = 'src/';
+const dest = 'dist/';
+
+// Bootstrap Sources
+const bootstrapSass = {
+	in: './node_modules/bootstrap-sass/'
+};
+
+// fonts
+const fonts = {
+	in: [source + 'fonts/*.*', bootstrapSass.in + 'assets/fonts/**/*'],
+	out: dest + 'fonts/'
+};
+
+// Our scss source folder: .scss files
+const scss = {
+    in: source + 'scss/main.scss',
+    out: dest + 'css/',
+    watch: source + 'scss/**/*',
+    sassOpts: {
+        outputStyle: 'nested',
+        precison: 3,
+        errLogToConsole: true,
+        includePaths: [bootstrapSass.in + 'assets/stylesheets']
+    }
+};
+
+// copy bootstrap required fonts to dest
+gulp.task('fonts', function () {
+    return gulp
+        .src(fonts.in)
+        .pipe(gulp.dest(fonts.out));
+});
+
+// compile scss
+gulp.task('sass', ['fonts'], function () {
+    return gulp.src(scss.in)
+        .pipe(sass(scss.sassOpts))
+        .pipe(gulp.dest(scss.out));
+});
+
+// default task
+gulp.task('default', ['sass'], function () {
+	gulp.watch(scss.watch, ['sass']);
+});
